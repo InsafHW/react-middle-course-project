0f7e6cb6d51038c8d6bb22cc24296363b4c85992
insaf.halirahmanov <insaf.halirahmanov@ispring.com>
2025-01-22 22:52:59 +0300

    Добавил роутинг

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
-import {Counter} from './components/Counter'
 import './index.scss'
+import {BrowserRouter, NavLink, Route, Routes} from 'react-router'
+import {AboutPageAsync} from './pages/aboutPage/AboutPage.async'
+import {MainPageAsync} from './pages/mainPage/MainPage.async'
+import {Suspense} from 'react'
 
 function App() {
     return (
-        <div className="app">
-            <div>TEXT</div>
-            <Counter />
-        </div>
+        <BrowserRouter>
+            <div className="app">
+                <NavLink to={'/'}>to main</NavLink>
+                <NavLink to={'/about'}>to about</NavLink>
+                <Routes>
+                    <Route
+                        path="/"
+                        element={(
+                            <Suspense key={'/'} fallback={<div>LOADING))</div>}>
+                                <MainPageAsync />
+                            </Suspense>
+                        )}
+                    />
+                    <Route
+                        path="/about"
+                        element={(
+                            <Suspense key={'/about'} fallback={<div>LOADING)))))</div>}>
+                                <AboutPageAsync />
+                            </Suspense>
+                        )}
+                    />
+                </Routes>
+            </div>
+        </BrowserRouter>
     )
 }
 
---
+++ src/pages/aboutPage/AboutPage.async.tsx
@@ -0,0 +1,14 @@
+import {lazy} from 'react'
+
+const AboutPageAsync = lazy(() =>
+    new Promise(resolve => {
+        setTimeout(() => {
+            // @ts-ignore
+            resolve(import('./AboutPage'))
+        }, 1500)
+    })
+)
+
+export {
+    AboutPageAsync,
+}
